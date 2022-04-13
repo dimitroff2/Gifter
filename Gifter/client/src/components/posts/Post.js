fe3a92e48e07bcd7f19cddd53b5488ab6f1bfc9e
dimitroff2 <dimitroff2@gmail.com>
2022-04-13 09:44:42 -0400

    finished react routing

--- Gifter/client/src/components/posts/Post.js
+++ Gifter/client/src/components/posts/Post.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Card, CardImg, CardBody } from "reactstrap";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../providers/UserProvider";
+
 
 const Post = ({ post }) => {
+  const {user} = useContext(UserContext)
   // put a question mark in front to force it to read null anyway
   return (
     <Card className="m-4">
-      <p className="text-left px-2">Posted by: {post.userProfile?.name}</p> 
+      
+      <p className="text-left px-2">Posted by:<Link to={`/user/${user.id}`}> {post.userProfile?.name}</Link></p> 
       
       <CardImg top src={post.imageUrl} alt={post.title} />
       <CardBody>
